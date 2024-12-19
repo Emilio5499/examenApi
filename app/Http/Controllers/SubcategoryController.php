7e94b5538f7167523b2a3dbce1b6e78ef3450600
DESKTOP-7VUTVPT\Emilio <1448592@alu.murciaeduca.es>
2024-12-19 16:41:56 +0100

    EJERCICIO 5

--- app/Http/Controllers/SubcategoryController.php
+++ app/Http/Controllers/SubcategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,19 @@ class SubcategoryController extends Controller
             ->select('name', 'description')
             ->$this->Category::name
             ->paginate(10);
+
+        return view('subcategories.index', compact('subcategories'));
+    }
+
+    public function AuthIndex()
+    {
+        abort_if(! auth()->user()->tokenCan('Subcategories-list'), 403);
+
+        $subcategories = Subcategory::with('category')
+            ->select('name', 'description')
+            ->$this->Category::name
+            ->$this->Product::name(Product::all()->pluck('name'))
+            ->paginate(12);
 
         return view('subcategories.index', compact('subcategories'));
     }
